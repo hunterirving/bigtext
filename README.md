26d1b450182d883179853754819930e1ebc5e0ed
Hunter <hunter@hunterirving.com>
2026-02-15 13:02:40 -0500

    add PWA requirements; add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# bigtext 📓
+
+A minimal tool for relaying information using photons. Type to display your message at the biggest possible size that will fit on your screen, then point it in the direction of your receiver.
+
+For offline use, install it from your web browser as a <a href="https://hunterirving.github.io/web_workshop/pages/pwa/">Progressive Web App</a>.
---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'bigtext-v1';
+const urlsToCache = [
+  '/bigtext/',
+  '/bigtext/index.html',
+  '/bigtext/resources/main.css',
+  '/bigtext/resources/main.js',
+  '/bigtext/manifest.json',
+  '/bigtext/resources/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              const responseClone = response.clone();
+              caches.open(CACHE_NAME)
+                .then(cache => cache.put(event.request, responseClone));
+            }
+            return response;
+          });
+      })
+  );
+});
